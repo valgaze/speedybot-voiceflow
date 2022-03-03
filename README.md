45b7419d72608ba834a9970ef6c96ef6c2dfaa48
Victor Algaze <valgaze@cisco.com>
2022-03-03 11:19:40 -0800

    Initial concept

---
+++ README.md
@@ -0,0 +1,74 @@
+## Speedybot-voiceflow
+
+## Quickstart
+
+## 1) WebEx token
+
+- If you don't have a bot, create one and save the token from here: **[https://developer.webex.com/my-apps/new/bot](https://developer.webex.com/my-apps/new/bot)**
+
+- Set WebEx token in **[settings/config.json](./settings/config.json)** under the ```token``` field
+
+## 2) Voiceflow Canvas api key
+
+- From your project tap the Voiceflow icon in the top right corner select **Project Settings** >> **Integrations**
+
+- Set VoiceFlow api key in **[settings/voiceflow.json](./settings/voiceflow.json)** under the ```apiKey``` field
+
+![img](./docs/assets/vf_key.png)
+
+## 3) Boot
+
+```
+npm run dev
+
+# yarn run dev
+```
+
+## Video Instructions
+
+- 101: https://share.descript.com/view/ds3UA1kUb9z
+
+## Commands
+
+| **Command** | **Desc** |
+| --- | --- |
+| ```npm run setup``` | Setup dependencies |
+| ```npm run dev``` | Start with live-reload on code-changes |
+| ```npm run ui``` | Boot web-based debug panel | 
+| ```npm start``` | Start chat agent |
+| ```npm start:server``` | Start server (you'll need to set ```webhookUrl``` in settings/config.json) |
+
+## Demo
+
+### Project
+
+![img](./docs/assets/canvas.png)
+
+### Agent
+
+![img](./docs/assets/demo.gif)
+
+
+
+<details>
+
+<summary>todo</summary>
+
+```
+## todo
+
+- docs
+
+    - quickstart
+
+    - *.vf file, instructions to import
+
+    - docs where to find canvas api key, save to settings/voiceflow.json
+
+- agent
+
+    - say no when asking for more ice cream at "end"
+
+```
+
+</details>
---
+++ settings/handlers.ts
@@ -0,0 +1,76 @@
+import { BotHandler, $, SpeedyCard} from 'speedybot'
+import { Voiceflow } from './vf'
+import { apiKey } from './voiceflow.json'
+
+const handlers: BotHandler[] = [
+	{
+		keyword: '<@catchall>',
+		async handler(bot, trigger) {
+			const $bot = $(bot)
+			const vf = new Voiceflow(apiKey)
+
+			let session = await $bot.getData('session')
+			if (!session) {
+				session = $bot.rando()
+				await $bot.saveData('session', session)
+			}
+
+			const res = await vf.send(session, trigger.text)
+
+			for (let i = 0; i < res.length; i++) {
+				const item = res[i]
+
+				const { type, value } = item
+				if (type === 'text') {
+					await bot.say(value)
+				}
+
+				if (type === 'choice') {
+					await $bot.sendChips(value as string[])
+				}
+
+				if (type === 'visual' && value) {
+					const card = new SpeedyCard()
+										.setImage(value as string)
+
+					await bot.sendCard(card.render(), value as string)
+				}
+			}
+		},
+		helpText: 'Runs for all inputs'
+	},
+	{
+		keyword: '<@fileupload>',
+		async handler(bot, trigger) {
+			const supportedFiles = ['json', 'txt', 'csv']
+            // take 1st file uploaded, note this is just a URL & not authenticated
+            const [file] = trigger.message.files
+
+            // Retrieve file data
+			const fileData = await $(bot).getFile(file)
+			const { extension, type } = fileData
+
+            if (supportedFiles.includes(extension)) {
+                const {data} = fileData
+                // bot.snippet will format json or text data into markdown format
+                bot.say({markdown: $(bot).snippet(data)})
+
+				// ex. Send data somewhere or to 3rd-party service
+            } else {
+                bot.say(`Sorry, somebody needs to add support to handle *.${extension} (${type}) files`)
+            }
+		},
+		helpText: 'A special handler that will activate whenever a file is uploaded'
+	},
+	{
+		keyword: '<@submit>',
+		handler(bot, trigger) {
+			// Ex. From here data could be transmitted to another service or a 3rd-party integrationn
+			bot.say(`Submission received! You sent us ${JSON.stringify(trigger.attachmentAction.inputs)}`)
+
+		},
+		helpText: `A special handler that fires anytime a user submits data (you can only trigger this handler by tapping Submit in a card)`
+	},
+]
+
+export default handlers
